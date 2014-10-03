e75c02608d48c7a4d558f14f2a880f647aef0f42
devshred <devshred@tigerflanke.de>
2014-10-03 10:37:47 +0200

    remove unused imports

--- spring-rest/src/test/java/org/devshred/rest/config/SpringTest.java
+++ spring-rest/src/test/java/org/devshred/rest/config/SpringTest.java
@@ -2,10 +2,8 @@ package org.devshred.rest.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
 
